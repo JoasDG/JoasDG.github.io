808a6879fabcf78e7a45cf3930da290e5ec0cb56
JoasDG <162144735+JoasDG@users.noreply.github.com>
2024-03-18 15:45:15 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function projectRedirect() {
+function redirectToPage() {
     window.location.href = "https://joasdg.github.io/vacationgenerator";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var elementToClick = document.getElementById("projects");
+    if (elementToClick) {
+        elementToClick.addEventListener("click", redirectToPage);
+    }
+});
